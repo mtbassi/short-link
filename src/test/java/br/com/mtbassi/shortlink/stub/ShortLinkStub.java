c7b4a48e3e0afb62e313f35e8a18a49e7e5329fc
Matheus Bassi <bassi.matheus@outlook.com>
2024-03-17 22:02:03 -0300

    test: create response stub

--- src/test/java/br/com/mtbassi/shortlink/stub/ShortLinkStub.java
+++ src/test/java/br/com/mtbassi/shortlink/stub/ShortLinkStub.java
@@ -1,6 +1,7 @@
 package br.com.mtbassi.shortlink.stub;
 
 import br.com.mtbassi.shortlink.domain.ShortLink;
+import br.com.mtbassi.shortlink.dto.ResponseDTO;
 
 import java.time.LocalDateTime;
 
@@ -9,6 +10,15 @@ public class ShortLinkStub {
     public static ShortLink valid() {
         return ShortLink.builder()
                 .shortLink("MTBASSI")
+                .originalLink("https://github.com/mtbassi")
+                .access(0)
+                .creationDate(LocalDateTime.of(2000, 8, 12, 0, 0, 0))
+                .build();
+    }
+
+    public static ResponseDTO responseDTO() {
+        return ResponseDTO.builder()
+                .shortLink("/shortlink/MTBASSI")
                 .originalLink("https://github.com/mtbassi")
                 .access(0)
                 .creationDate(LocalDateTime.of(2000, 8, 12, 0, 0, 0))
