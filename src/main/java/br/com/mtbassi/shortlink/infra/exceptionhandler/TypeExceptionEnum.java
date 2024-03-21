143e9cc96f2e6870251a68017905445b7dfa2650
Matheus Bassi <bassi.matheus@outlook.com>
2024-03-20 23:14:55 -0300

    feat: implements exception handling

--- src/main/java/br/com/mtbassi/shortlink/infra/exceptionhandler/TypeExceptionEnum.java
+++ src/main/java/br/com/mtbassi/shortlink/infra/exceptionhandler/TypeExceptionEnum.java
@@ -10,7 +10,8 @@ public enum TypeExceptionEnum {
 
     INVALID_URL_EXCEPTION(HttpStatus.BAD_REQUEST, "URL invalid.", "URL address must start with 'https://' or 'http://'"),
     ORIGINAL_LINK_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "Original link not found.", "Parameter not found, try again."),
-    GENERATE_SHORT_LINK_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Error generating short link.", "Error generating short link, try again later.");
+    GENERATE_SHORT_LINK_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Error generating short link.", "Error generating short link, try again later."),
+    GENERATE_QR_CODE_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Error generating qr code.", "Error generating qr code, try again later.");
 
     private final HttpStatus status;
     private final String error;
--- src/main/java/br/com/mtbassi/shortlink/service/QrCodeService.java
+++ src/main/java/br/com/mtbassi/shortlink/service/QrCodeService.java
@@ -1,10 +1,11 @@
 package br.com.mtbassi.shortlink.service;
 
+import br.com.mtbassi.shortlink.infra.exceptionhandler.ErrorCustomException;
+import br.com.mtbassi.shortlink.infra.exceptionhandler.TypeExceptionEnum;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -12,11 +13,14 @@ import java.io.ByteArrayOutputStream;
 @Service
 public class QrCodeService {
 
-    @SneakyThrows
-    public byte[] generateQrCode(String originalLink){
-        BitMatrix bitMatrix = new QRCodeWriter().encode(originalLink, BarcodeFormat.QR_CODE, 200, 200);
+    public byte[] generateQrCode(String originalLink) {
         var outputStream = new ByteArrayOutputStream();
-        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
-        return outputStream.toByteArray();
+        try {
+            BitMatrix bitMatrix = new QRCodeWriter().encode(originalLink, BarcodeFormat.QR_CODE, 200, 200);
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            throw new ErrorCustomException(TypeExceptionEnum.GENERATE_QR_CODE_EXCEPTION, e.getMessage());
+        }
     }
 }
